ff0a8c9b84c85c8fc2d5f4d18076c979e9cbc894
d33zhou <d33zhou@outlook.com>
2021-11-22 20:14:48 +0000

    Add new head.js function

--- assertEqual.js
+++ assertEqual.js
@@ -2,19 +2,19 @@
 // writes error msg to console if assertion false, otherwise nothing
 
 const assertEqual = function(actual, expected) {
-  let evalEqual = "";
+  let evalEqual;
   
-  actual === expected ? evalEqual += "✅✅✅ Assertion Passed: " + actual + " === " + expected : evalEqual += "🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected;
+  actual === expected ? evalEqual = `✅✅✅ Assertion Passed: ${actual} === ${expected}` : evalEqual = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
 
   console.log(evalEqual);
 };
 
 
 //TEST CASES
-/*
+/* 
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 
 assertEqual("Labs", "Labs");
 assertEqual(1, 2);
-*/
+ */
---
+++ head.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  let evalEqual;
+  
+  actual === expected ? evalEqual = `✅✅✅ Assertion Passed: ${actual} === ${expected}` : evalEqual = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+
+  console.log(evalEqual);
+};
+
+
+// create function head which returns the first item in the array (the element, not as an array)
+function head(input) {
+  return input[0];
+}
+
+
+//TEST CASES
+/* assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([5]), 5);
+assertEqual(head([]), undefined); */
