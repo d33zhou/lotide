85319e9c356ae0878d59f6a8ca00e3076ba70ae7
d33zhou <d33zhou@outlook.com>
2021-11-23 21:17:31 +0000

    Add assertArraysEqual.js function

---
+++ assertArraysEqual.js
@@ -0,0 +1,31 @@
+const eqArrays = function(arr1, arr2) {
+  let same = true;
+
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      arr1[i] !== arr2[i] ? same = false : "";
+    }
+  } else {
+    same = false;
+  }
+
+  return same;
+}
+
+const assertArraysEqual = function (actual, expected) {
+  let evalEqual;
+  
+  eqArrays(actual, expected) ? evalEqual = `✅✅✅ Assertion Passed: ${actual} === ${expected}` : evalEqual = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+
+  console.log(evalEqual);
+};
+
+
+// TEST CASES
+assertArraysEqual([1, 2, 3], [1, 2, 3]); //true
+assertArraysEqual([1, 2, 3], [1, 2, 4]); //false
+assertArraysEqual([1, 2, 3], [1, 2, 3, 4]); //false
+assertArraysEqual([], []); //true
+assertArraysEqual([1], []); //false
+assertArraysEqual([], [1]); //false
+assertArraysEqual(["1"], [1]); //false
