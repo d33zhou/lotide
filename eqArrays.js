896ffea84fc5acb592746fc2f5ae0ca7ef85c42b
d33zhou <d33zhou@outlook.com>
2021-11-27 00:16:39 +0000

    add recursion for nested arrays in function eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -7,23 +7,38 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2) {
-  let same = true;
-
   if (arr1.length === arr2.length) {
     for (let i = 0; i < arr1.length; i++) {
-      arr1[i] !== arr2[i] ? same = false : "";
+      if (Array.isArray(arr1[i])) {
+        if (!eqArrays(arr1[i], arr2[i])) {
+          return false;
+        }
+      } else {
+        if (arr1[i] !== arr2[i]) {
+          return false;
+        }
+      }
     }
   } else {
-    same = false;
+    return false;
   }
 
-  return same;
+  return true;
 };
 
 
 // TEST CASES
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
+// RECURSION TEST CASES
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true)
+assertEqual(eqArrays([[[[[[1]]]]]], [[[[[[1]]]]]]), true)
+assertEqual(eqArrays([1, [2, [3, [4]]]], [1, [2, [3, [4]]]]), true)
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false)
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false)
+assertEqual(eqArrays([[[[[[1]]]]]], [[[[[[2]]]]]]), false)
