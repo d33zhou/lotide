12d32a608c52e32eb14f662194f117a90181dd10
d33zhou <d33zhou@outlook.com>
2021-11-29 18:44:34 +0000

    refactor middle to modularize

--- middle.js
+++ middle.js
@@ -1,25 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  let same = true;
-
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      arr1[i] !== arr2[i] ? same = false : "";
-    }
-  } else {
-    same = false;
-  }
-
-  return same;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  let evalEqual;
-  
-  eqArrays(actual, expected) ? evalEqual = `✅✅✅ Assertion Passed: ${actual} === ${expected}` : evalEqual = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
-
-  console.log(evalEqual);
-};
-
 // return middle 2 elements of array (will return 2 elements if even length array, otherwise 1)
 const middle = function(arr) {
   let mid = [];
@@ -31,13 +9,4 @@ const middle = function(arr) {
   return mid;
 };
 
-
-// TEST CASES
-assertArraysEqual(middle([1]), []); // => true
-assertArraysEqual(middle([1, 2]), []); // => true
-
-assertArraysEqual(middle([1, 2, 3]), [2]); // => true
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => true
-
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => true
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => true
+module.exports = middle;
